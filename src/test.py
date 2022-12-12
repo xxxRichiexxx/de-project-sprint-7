436a0aef2b52082455a4cbb664978b96541b025c
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-12-13 00:43:20 +0300

    v0

--- src/test.py
+++ src/test.py
@@ -4,10 +4,26 @@ from pyspark.sql.functions import lit
 from pyspark.sql.window import Window 
 
 import pandas as pd
+import datetime as dt
 
+
+class numerator:
+    def __init__(self):
+        self.value = 0
+    def next_val(self):
+        self.value += 1
+        return self.value
+    def cur_val(self):
+        return self.value
+    
+n = numerator()  
 
 base_url = 'hdfs://rc1a-dataproc-m-dg5lgqqm7jju58f9.mdb.yandexcloud.net:8020'
-events_path = '/user/master/data/geo/events'      
+events_path = '/user/master/data/geo/events'
+
+def input_event_paths(date, depth):
+    day = dt.datetime.strptime(date, '%Y-%m-%d')
+    return [f"{base_url}{events_path}/date={(day-dt.timedelta(days=x)).strftime('%Y-%m-%d')}/" for x in range(int(depth))]
         
 spark = SparkSession.builder \
             .appName('geoProject') \
@@ -25,12 +41,12 @@ cities = spark.createDataFrame(cities) \
                 .cache()
 
 
-events = spark.read.parquet(base_url + events_path) \
-                        .sample(fraction=0.001) \
+events = spark.read.parquet(*input_event_paths('2022-06-21',10)) \
                         .withColumnRenamed('lat', 'lat2') \
                         .withColumnRenamed('lon', 'lng2') \
-                        
-events.write.mode("overwrite").parquet(base_url+'/user/andreydzr/data/tmp/events_geo_sample')
+                        .cache()
+
+events.printSchema()                        
 
 events_and_cities = events.join(cities)
 
@@ -57,14 +73,40 @@ events_and_cities = events_and_cities\
                         .withColumn('rank', F.rank().over(window))\
                         .where(F.col('rank') == 1) \
                         .cache()
+events_and_cities.show()
 
-user_home = events_and_cities\
+window = Window().partitionBy('event.message_from').orderBy(F.desc('event.message_ts'))
+
+act_city = events_and_cities\
                 .where(F.col('event_type') == 'message')\
                 .select(
                     'event.message_from',
-                    'event_type',
-                    'date',
-                    'city',
+                    F.first('city',True).over(window).alias('act_city')
                 )
 
-user_home.show()
+act_city.show()
+
+window = Window().partitionBy('message_from').orderBy('date')
+window_2 = Window().partitionBy('message_from').orderBy('date')
+              
+
+home_city = events_and_cities\
+                .where(F.col('event_type') == 'message')\
+                .select(
+                    'event.message_from',
+                    F.to_date('event.message_ts').alias('date'),
+                    'city'                   
+                ) \
+                .distinct() \
+                .withColumn('prev_city', F.lag('city').over(window))\
+                .withColumn(
+                    'visit', 
+                    F.when(
+                        (F.col('city')!=F.col('prev_city'))|(F.col('prev_city').isNull()),
+                        F.monotonically_increasing_id()
+                    )
+                ) \
+                .withColumn('visit_2', F.max('visit').over(window_2)) \
+                .groupBy('message_from', 'city', 'visit_2').count() \
+                
+                
